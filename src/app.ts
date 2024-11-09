817c128c5366d5d2bd250e103ae56771d786e74e
thectogeneral <salemolorundare@gmail.com>
2024-11-09 19:54:43 +0100

    fix/api-connection-error

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,29 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import path from "path";
 
+// local imports
 import { ErrorHandler } from "./middleware";
 import { authRoutes, walletRoutes } from "./routes";
-
+import { Logger } from "./utils";
 
 const app: Express = express();
 const allowlist = ["http://localhost:3000", process.env.FRONT_END_URL];
 
-const corsOptionsDelegate = (req: Request, callback: (err: any, corsOptions: any) => void) => {
-    const origin = req.header("Origin") || "";
-    const corsOptions = allowlist.includes(origin) ? { origin: true } : { origin: false };
+const corsOptionsDelegate = function (
+    req: Request,
+    callback: (err: any, corsOptions: any) => void
+) {
+    let corsOptions: any;
+    if (allowlist.indexOf(req.header("Origin")) !== -1) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+    }
     callback(null, corsOptions);
 };
 
 app.use(cors(corsOptionsDelegate));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,10 +33,12 @@ app.use(express.static("public"));
 // app.get("/api/", (req: Request, res: Response) => {
 //     Logger.info(req.body);
 //     return res.status(200).json({
-//         message: "Demo Credit API index page",
+//         message: "You have reached Demo Credit API index page",
 //         success: true,
 //     });
 // });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", authRoutes);
 app.use("/api", walletRoutes);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import dotenv from "dotenv";
 
+// locals
 import app from "./app";
 import { Logger } from "./utils";
-
+import { createKnexConnection } from "../config";
 
 dotenv.config();
+
+// Uncaught Exceptions
+process.on("uncaughtException", (err) => {
+    Logger.error(err);
+    process.exit(1);
+});
+
+createKnexConnection();
+
+if (!process.env.APP_PORT) {
+    process.exit(1);
+}
 
 exports.APP_PORT = parseInt(process.env.APP_PORT, 10);
 
 const server = app.listen(exports.APP_PORT, () => {
     Logger.info(`🚀 Server ready at port: ${exports.APP_PORT}`);
+});
+process.on("unhandledRejection", (err) => {
+    Logger.error(err);
+    server.close(() => {
+        process.exit(1);
+    });
 });
